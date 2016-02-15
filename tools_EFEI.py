54eb8fe10abadc305d5a9ac43b7f0defd5f4607e
DANIEL RIVERO <darthquevedus@gmail.com>
2016-02-15 15:38:54 +0100

    just planning tools...

---
+++ tools_EFEI.py
@@ -0,0 +1,31 @@
+import numpy as np
+from numpy.linalg import norm
+
+
+
+
+
+def read_xyz(filename):
+
+    return
+
+
+def read_force(filename):
+    
+
+    return
+
+
+
+def construct_fvector(xyz,modF):
+    return
+
+
+
+def new_xyz(xyz,fvector):
+
+    return xyz
+
+
+
+
